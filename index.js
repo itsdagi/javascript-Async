178b35ad4929762f50c1836efc6966646efcf80a
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-08 13:17:08 +0000

    callback

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ console.log("Start")
  function loginUser(email, password, callBack){
   setTimeout(()=>{
     callBack({userID:564, status: "success"})
-    
-  },4000);  
+    console.log("User logged in" + email)
+  },2000);  
 }
 
-function getUserTweets(){}
-function getTweetComments(){}
-const data = loginUser("xcvkp@example.com", "password",(values) => console.log(values));
+function getUserTweets(userID, callBack){
+  setTimeout(()=>{
+    tweetData = [{tweetID:1, content:"ABC"}, {tweetID:2, content:"XYZ"}]
+    callBack(tweetData);
+    console.log("tweet data")
+    },1000)
+}
+function getTweetComments(tweetID, callBack){
+  setTimeout(()=>{
+    commentData=[{commentID:1, content:"COOL!"}]
+   callBack(commentData);
+  },1000)
+}
+const data = loginUser("xcvkp@example.com","dagim",(userData) =>{
+  getUserTweets(userData.userID, ()=>{
+    getTweetComments(tweetData[0].tweetID,()=>{
+      console.log("callback")
+    })
+  });
+});
 
 
 console.log("Finish");
