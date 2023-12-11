23260d06b0cb205c9c6e2af890d52abd464243af
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-11 15:06:08 +0000

    body

--- fetching.js
+++ fetching.js
@@ -13,7 +13,7 @@
 async function getPosts(){
   const response = await fetch('https://jsonplaceholder.typicode.com/posts/')
   const posts = await response.json()
-  posts.forEach(post => console.log(post.title))
+  posts.forEach(post => console.log(post.body))
 }
 
 getPosts();
